e988bf102f9d0d425e1f0f80fd9c0fe71467fbdb
offlinebot <flintstonelg@gmail.com>
2025-05-16 22:12:37 +0200

    nvim fix

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", "<Cmd>Neotree filesystem toggle right<CR>", {})
+		vim.keymap.set("n", "<leader>bf", "<Cmd>Neotree buffers reveal float<CR>", {})
+	end,
+}
