3cf14d2b33dff4353620a73d70b9f7f987a813f1
offlinebot <flintstonelg@gmail.com>
2025-05-17 15:48:16 +0200

    nvim show . files

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,14 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                },
+            },
+        })
 		vim.keymap.set("n", "<C-n>", "<Cmd>Neotree filesystem toggle right<CR>", {})
 		vim.keymap.set("n", "<leader>bf", "<Cmd>Neotree buffers reveal float<CR>", {})
 	end,
